9210c40b6ee607a45413d9d6b94f1a4fc30492a6
ackarlse <anncharlottkarlsen@ymail.com>
2024-02-27 14:41:43 +0100

    Jeg jobber i app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <h1>Ann-Charlott og Marius sin side!</h1>
+    <>
+      <h1>Ann-Charlott og Marius sin side!</h1>
+      <p>Her jobber Ann-Charlott</p>
+    </>
   )
 }
 
